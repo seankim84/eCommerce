28ed4f323f00033f476d7a15b87f84ef558cdec9
SeanKim <sean.kim84@gmail.com>
2018-09-27 13:14:10 +0700

    Fetching the Detail

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9e52eb6b3aa765edc16";
+/******/ 	var hotCurrentHash = "0da0cd796f0265c041d4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/movie.js
+++ pages/movie.js
@@ -1,18 +1,29 @@
-import Head from 'next/head';
-import { withRouter } from 'next/router';
+import Head from "next/head";
+import { withRouter } from "next/router";
+import Axios from "axios";
 
 const Movie = props => (
-    <div>
-        <Head>
-            <title>{props.router.query.title} | Sean Store</title>
-        </Head>
-        <h1>{props.title}</h1>
-        <p>Lorem Ipsum</p>
-    </div>
+  <div>
+    <Head>
+      <title>{props.movie.title} | Sean Store</title>
+    </Head>
+    <h1>{props.movie.title}</h1>
+    <p>{props.movie.description_intro}</p>
+  </div>
 );
 
-Movie.getInitialProps = async () => {
-    return { title: "lalalalalalalalalal" }
-}
+Movie.getInitialProps = async context => {
+  const {
+    query: { id }
+  } = context;
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await Axios.get(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  );
+  return { movie };
+};
 
 export default withRouter(Movie);
